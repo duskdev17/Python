4e93de940d77c45c9d34138c8c03b0331b41dde6
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-11-02 19:33:17 +0600

    update

--- Practice/main.py
+++ Practice/main.py
@@ -1,16 +1,3 @@
-s = [1, 3, 0, 5, 8, 5]
-f = [2, 4, 6, 7, 9, 9]
+a = str(input("Enter your name: "))
 
-
-n = len(s)
-A = [0]
-i=0
-
-for m in range(1, n):
-    if s[m] >= f[i]:
-        A.append(m)
-
-print("Selected Activities: ")
-
-for element in A:
-    print(element, end='    ')
+print(a)
