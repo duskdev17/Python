ad50310f6ea9e9bf684dd91a075353e622403c08
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-11-19 20:15:12 +0600

    greedy algorithm

--- Practice/2. Assignment#2.py
+++ Practice/2. Assignment#2.py
@@ -1,17 +1,18 @@
-"""Shortest interval -- Greedy Algorithm"""
+"""Earliest Start Time --- Greedy Algorithm"""
+
 activity = [(12 , 15 ), (0 , 9 ), (0 , 11 ), (5 , 15 ), (9 , 13 ),( 0 , 14 ), ( 4 , 12 )]
 
 n = len(activity)
-
-activity.sort(key=lambda x: x[1])
+sorted_activities = sorted(activity)
+print("Sorted by Earliest Start Time: ", sorted_activities)
 
 selected = []
 i = 0
 selected.append(activity[i])
 
 for j in range(1, n):
-    if activity[j][0] >= activity[i][1]:
-        selected.append(activity[j])
+    if sorted_activities[j][0] >= sorted_activities[i][1]:
+        selected.append(j)
         i = j
 
 print("Selected Activities: ", end = '  ')
