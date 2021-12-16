b61f70b1866eccd4896f10fa1b2afd6d7ea892ca
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-12-16 23:12:08 +0600

    update

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,37 +1,11 @@
-def dfs(u):
-    color[u] = 'gray'
+list = ['apple', 'banana']
+dict = {'name' : 'anik', 'age' : 12, 'time' : 12.30}
+list_inside_dict = {'list' : [1, 2, 3, 4]}
 
-    for element in adj[u]:
-        if color[element] == 'white':
-            dfs(element)
+print('List inside a dict: ', list_inside_dict)
 
-    color[u] = 'black'
-    print(u, end=' ')
+print(dict)
+print('Name:',dict['name'])
 
-
-# as we want to allow any vertex name not only 0 to n-1
-# we need have complete list of all vertices name
-nodes = input("Enter nodes' name: ").split()
-e = int(input("Number of edges: "))
-
-# creating dictionary to store adjacency list of each vertex
-adj = {}
-# creating a dictionary to store color's value of each vertex
-color = {}
-# adding all vertex name as key and an empty list as corresponding value
-# adding white color to each vertex
-for element in nodes:
-    adj[element] = []
-    color[element] = 'white'
-
-# creating adjacency list
-for i in range(e):
-    u, v = input("Edge %d: " % (i + 1)).split()
-    adj[u].append(v)
-    # if graph is directed then you must eliminate the following line
-    # adj[v].append(u)
-
-
-for name in nodes:
-    if color[name] == 'white':
-        dfs(name)
+for items in list:
+    print(items)
