857e6ef17d3f1cad8f47fe06b3f1c7754c2f1ed1
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-01-06 12:04:33 +0600

    happ_new_year

--- GoodBye 2021.py
+++ GoodBye 2021.py
@@ -4,5 +4,5 @@ for i in range(2021):
     print("Goodbye 2021")
 
 
-for i in range(2020):
-    print("**Happy New Year**")
+for i in range(2022):
+    print("**Happy New Year 2022**")
---
+++ Welcome 2022.py
@@ -0,0 +1,4 @@
+print("Happy New Year")
+
+for i in range(2022):
+    print("Welcome 2022")
